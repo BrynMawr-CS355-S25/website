8c7894a8ba324959f6758a9d28f8b4f0428d4487
alinen <anormoyle@brynmawr.edu>
2025-01-29 12:27:31 -0500

    clarify microcat

--- build.sh
+++ build.sh
@@ -1,5 +1,10 @@
 #! /bin/bash
 
+if [ $# -ne 1 ]; then
+  echo "usage: $0 <message>"
+  exit 0
+fi
+
 /usr/local/bin/asciidoctor assts/*.adoc *.adoc
 git add .
-git commit -m "tweaks"
+git commit -m "$1"
