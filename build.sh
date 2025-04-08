61aeb209d85d0943fc228c49b1decacd78852722
alinen <anormoyle@brynmawr.edu>
2025-04-08 14:55:19 -0400

    update writeup

--- build.sh
+++ build.sh
@@ -1,10 +1,11 @@
 #! /bin/bash
 
-if [ $# -ne 1 ]; then
-  echo "usage: $0 <message>"
-  exit 0
-fi
 
 /usr/local/bin/asciidoctor assts/*.adoc *.adoc labs/*.adoc
-git add .
-git commit -m "$1"
+if [ $# -ge 1 ]; then
+  git add .
+  git commit -m "$1"
+fi
+if [ $# -ne 1 ]; then
+  echo "specify message to commit changes"
+fi
